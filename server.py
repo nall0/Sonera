7640b731ad7b931839414e84baf514a546c50b8c
mmarie4 <max.marie.4@gmail.com>
2017-04-05 09:29:16 +0200

    Creation de server.py avec Flask +  la creation de la base de données via SQLAlchemy

---
+++ server.py
@@ -0,0 +1,48 @@
+from flask import *
+from sqlalchemy import *
+from sqlalchemy.sql import *
+
+# -------- Create SQL tables ----------
+engine = create_engine('sqlite:///sonera.db', echo=True)   
+metadata = MetaData()                                     
+users = Table('users', metadata,                          
+            Column('id', Integer, autoincrement=True, primary_key=True),
+            Column('first_name', String),
+            Column('last_name', String),
+            Column('password', String))
+metadata.create_all(engine)                               
+connection = engine.connect()
+
+# ------- Create Flask server ---------
+app = Flask(__name__)
+app.secret_key = 'stytjyntil468kyjnmti65468'  
+
+# ------- Routes ---------
+@app.route('/')
+def index():
+  logged = 'logged' in session                                
+  if logged:
+    txt = 'Bonjour %s !' % session['id']
+  return render_template('index_sessions.html', message=txt, logged=logged)
+
+@app.route('/login', methods=['POST'])
+def login():
+  session['id'] = escape(request.form['id'])              
+  session['logged'] = True
+  return redirect('/')
+
+@app.route('/logout')
+def logout():
+  session.clear()
+  return redirect('/')
+
+if __name__ == '__main__':
+  app.run(debug=True)
+  
+  
+# ------- Function addUser() --------
+def addUser(first_name, last_name, password):
+	connection.execute(users.insert())
+	
+
+connection.close() 
