4960eeb4fa714f19b7f160d07f8d5dc2d71662d3
mmarie4 <max.marie.4@gmail.com>
2017-04-12 11:55:10 +0200

    modif templates, ajout des routes home et profile (pas fini)

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import *
 from database import db_session
 
+
+# website for SQLAlchemy+Flask (declarative base) : http://flask.pocoo.org/docs/0.12/patterns/sqlalchemy/
+
 nbUsers=0
 # ------- Function countNbUsers --------------------
 def countNbUsers():
@@ -14,11 +17,11 @@ def countNbUsers():
 		nbUsers += 1
 	return nbUsers
 # ------- Function addUser() -----------------------
-def addUser(first_name, last_name, password):
+def addUser(first_name, last_name, password, biography):
 	from database import db_session
 	from users import User
 	global nbUsers
-	new_user = User(first_name, last_name, password)
+	new_user = User(first_name, last_name, password, biography)
 	db_session.add(new_user)
 	db_session.commit()
 	nbUsers += 1
@@ -36,18 +39,20 @@ app.secret_key = 'stytjyntil468kyjnmti65468'
 # ------- Routes ----------------------------------
 @app.route('/')
 def index():
-	txt = "Salut"
 	logged = 'logged' in session  
 	from users import User 
 	list_users = User.query.all()
-	if logged:
-		txt = "Salut %d" % nbUsers                       
-	return render_template('sigin.html', logged=logged, message=txt, users=list_users)
+	#if logged:
+		#return redirect('/home')                      
+	return render_template('sigin.html', logged=logged, users=list_users)
 
 @app.route('/login', methods=['POST'])
 def login():
 	session['id'] = escape(request.form['id'])
 	session['password'] = escape(request.form['password_sigin'])
+	from users import User
+	current_user = User.query.filter(User.id == session['id']).first()
+	session['biography'] = current_user.biography
 	session['logged'] = True
 	return redirect('/')
 
@@ -61,15 +66,24 @@ def signup():
 	session['password'] = escape(request.form['password_sigup'])
 	session['first_name'] = escape(request.form['first_name'])
 	session['last_name'] = escape(request.form['last_name'])
-	addUser(session['first_name'], session['last_name'], session['password'])
+	session['biography'] = escape(request.form['biography'])
+	addUser(session['first_name'], session['last_name'], session['password'], session['biography'])
 	session['id'] = nbUsers
 	session['logged'] = True
 	return redirect('/')
 
+@app.route('/profile0')
+def profile0():
+	from users import User
+	current_user = User.query.filter(User.id == session['id']).first()
+	bio = current_user.biography
+	return render_template('profile0.html', first_name=session['first_name'], last_name=['last_name'], biography=bio)
+	
 @app.route('/home', methods=['POST'])
 def home():
-    
-    return render_template('home.html', first_name=, last_name=, , biography=)
+	return render_template('home.html', first_name=session['first_name'], last_name=['last_name'])
+	
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- users.py
+++ users.py
@@ -7,11 +7,13 @@ class User(Base):
 	first_name = Column(String)
 	last_name = Column(String)
 	password = Column(String)
+	biography = Column(String)
 
-	def __init__(self, first_name=None, last_name=None, password=None):
+	def __init__(self, first_name=None, last_name=None, password=None, bio=None):
 		self.first_name = first_name
 		self.last_name = last_name
 		self.password = password
+		self.biography = bio
 
 	def __repr__(self):
-		return '<User %s, %s, %s>' % (self.id, self.first_name, self.last_name)
+		return '<User %s, %s, %s, biography : %s>' % (self.id, self.first_name, self.last_name, self.biography)
