6ab2b22b1d61319c7af468cb217de2c588eae4c0
mmarie4 <max.marie.4@gmail.com>
2017-04-19 13:56:10 +0200

    Ajout pseudo + verification pseudo et mdp + pseudo devient la clé primaire + legère modif des templates

--- server.py
+++ server.py
@@ -18,18 +18,18 @@ def countNbUsers():
 		nbUsers += 1
 	return nbUsers
 # ------- Function addUser() -----------------------
-def addUser(first_name, last_name, password, biography):
+def addUser(pseudo, first_name, last_name, password, biography):
 	from database import db_session
 	from users import User
 	global nbUsers
-	new_user = User(first_name, last_name, password, biography)
+	new_user = User(pseudo, first_name, last_name, password, biography)
 	db_session.add(new_user)
 	db_session.commit()
 	nbUsers += 1
 # ---------------- Methode which test the password ------------------------
-def pw_verification(password):
+def pw_verification(pseudo, password):
 	from users import User
-	current_user = User.query.filter(User.id == session['id']).first()
+	current_user = User.query.filter(User.pseudo == session['pseudo']).first()
 	real_password = current_user.password
 	if real_password == password:
 		return True
@@ -45,27 +45,28 @@ init_db()
 
 # ------- Create Flask server ----------------------
 app = Flask(__name__)
-app.secret_key = 'stytjyntil468kyjnmti65468'  
+app.secret_key = 'stytjyntil468kyjnmti65468'
 
 
 # ------- Routes ----------------------------------
 @app.route('/')
 def index():
-	logged = 'logged' in session 
+	logged = 'logged' in session
 	nbUsers = countNbUsers()
 	if logged:
-		return redirect('/home')                      
+		return redirect('/home')
 	return render_template('sigin.html', logged=logged, nbUsers=nbUsers)
 
 @app.route('/login', methods=['POST'])
 def login():
-	session['id'] = escape(request.form['id'])
+	session['pseudo'] = escape(request.form['pseudo'])
 	session['password'] = escape(request.form['password_sigin'])
 	from users import User
-	current_user = User.query.filter(User.id == session['id']).first()
+	current_user = User.query.filter(User.pseudo == session['pseudo']).first()
 	if current_user == None:
 		return redirect('/')
-	elif pw_verification(session['password']):
+	elif pw_verification(session['pseudo'], session['password']):
+		session['id'] = current_user.id
 		session['first_name'] = current_user.first_name
 		session['last_name'] = current_user.last_name
 		session['biography'] = current_user.biography
@@ -76,14 +77,15 @@ def login():
 def logout():
 	session.clear()
 	return redirect('/')
-	
+
 @app.route('/signup', methods=['POST'])
 def signup():
+	session['pseudo'] = escape(request.form['pseudo'])
 	session['password'] = escape(request.form['password_sigup'])
 	session['first_name'] = escape(request.form['first_name'])
 	session['last_name'] = escape(request.form['last_name'])
 	session['biography'] = escape(request.form['biography'])
-	addUser(session['first_name'], session['last_name'], session['password'], session['biography'])
+	addUser(session['pseudo'], session['first_name'], session['last_name'], session['password'], session['biography'])
 	session['id'] = nbUsers
 	session['logged'] = True
 	return redirect('/')
@@ -94,11 +96,11 @@ def profile0():
 	current_user = User.query.filter(User.id == session['id']).first()
 	bio = current_user.biography
 	return render_template('profile0.html', first_name=session['first_name'], last_name=['last_name'], biography=bio)
-	
+
 @app.route('/home', methods=['POST', 'GET'])
 def home():
-	return render_template('home.html', first_name=session['first_name'], last_name=session['last_name'], id=session['id'])
-# ------------------------------------------------------------------------------------------------	
+	return render_template('home.html', first_name=session['first_name'], last_name=session['last_name'], pseudo=session['pseudo'])
+# ------------------------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
@@ -108,6 +110,3 @@ if __name__ == '__main__':
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
-    
-
-
--- users.py
+++ users.py
@@ -3,17 +3,19 @@ from database import Base
 
 class User(Base):
 	__tablename__ = 'users'
-	id = Column(Integer, primary_key=True, autoincrement=True)
+	id = Column(Integer, autoincrement=True)
+	pseudo = Column(String, primary_key=True)
 	first_name = Column(String)
 	last_name = Column(String)
 	password = Column(String)
 	biography = Column(String)
 
-	def __init__(self, first_name=None, last_name=None, password=None, bio=None):
+	def __init__(self, pseudo=None, first_name=None, last_name=None, password=None, bio=None):
 		self.first_name = first_name
 		self.last_name = last_name
 		self.password = password
+		self.pseudo = pseudo
 		self.biography = bio
 
 	def __repr__(self):
-		return '<User %s, %s, %s, biography : %s>' % (self.id, self.first_name, self.last_name, self.biography)
+		return '<User %s, %s, %s, biography : %s>' % (self.pseudo, self.first_name, self.last_name, self.biography)
